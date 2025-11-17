141465d2a200e5801dab16991c58fd2a35c6bdfe
Rishad1404 <rishadislam2019@gmail.com>
2025-11-17 19:29:46 +0600

    first commit

---
+++ src/problem1.ts
@@ -0,0 +1,17 @@
+
+const formatValue=(value: string | number | boolean) : string | number | boolean =>{
+    if(typeof value === 'string'){
+        return value.toUpperCase();
+    }
+
+    if (typeof value === 'number'){
+        return value*10;
+    }
+    if (typeof value === 'boolean'){
+        return !value;
+    }
+}
+
+console.log(formatValue('hello'));
+console.log(formatValue(5));
+console.log(formatValue(true));
