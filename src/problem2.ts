7bb5411c2836d48175afdb7e2b156463f392c29c
Rishad1404 <rishadislam2019@gmail.com>
2025-11-17 19:44:16 +0600

    problem 2

---
+++ src/problem2.ts
@@ -0,0 +1,12 @@
+const getLength=(value:string | any[]) : number=>{
+    if(typeof value === 'string'){
+        return value.length;
+    }
+    else{
+        return value.length;
+    }
+
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
